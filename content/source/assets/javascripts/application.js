7629aabbef5bd621df878e1138780954a54bcd6b
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-04-02 15:50:31 -0700

    Add quick-nav component for revealing structure of large pages
    This commit adds a small drop-down menu at the top of each page. When clicked,
    it reveals an in-page hierarchical TOC which you can use to jump directly to any
    of the H2, H3, or H4 headers in the primary content. The TOC can be dismissed by
    clicking outside it or by re-clicking the trigger.
    
    This re-uses the SCSS from the top-nav dropdown, and uses a little bit of JQuery
    to do the rest of the lifting. I ruled out using Ruby to get the list of headers
    during the static build for a couple of reasons:
    
    - It would be significantly harder to accomplish.
    - It would be a dead-end investment, since we're moving away from the Middleman
      platform.
    - It risks a nasty effect on site build times and/or HTML munging code.
      (Previous experiments at using Nogokiri to safely extract what I want from the
      content HTML have not been good, but doing it with regexps can be even worse.)
    
    The extra event handlers to make click-based activation/deactivation work
    wouldn't be necessary if I'd just used hover-based activation like in the top
    navs, but that felt awkward once the TOC reached about the length of a screen.

--- content/source/assets/javascripts/application.js
+++ content/source/assets/javascripts/application.js
@@ -13,3 +13,32 @@ document.addEventListener("turbolinks:load", function() {
     docsSidebar.find("ul.nav").addClass("nav-hidden");
     docsSidebar.find("li").has("a.current-page").addClass("active");
 });
+
+document.addEventListener("turbolinks:load", function() {
+    "use strict";
+    $("#inner #inner-quicknav").html(
+        '<span id="inner-quicknav-trigger">Quick Nav<svg width="9" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M8.811 1.067a.612.612 0 0 0 0-.884.655.655 0 0 0-.908 0L4.5 3.491 1.097.183a.655.655 0 0 0-.909 0 .615.615 0 0 0 0 .884l3.857 3.75a.655.655 0 0 0 .91 0l3.856-3.75z" fill-rule="evenodd"/></svg></span><ul class="dropdown"></ul>'
+    );
+    var quickNavTrigger = $('#inner-quicknav #inner-quicknav-trigger');
+    var quickNav = $('#inner-quicknav > ul.dropdown');
+    var headers = $('#inner').find('h2, h3, h4');
+    headers.each( function(index, element) {
+        var level = element.nodeName.toLowerCase();
+        var header_text = $(element).text();
+        var header_id = $(element).attr('id');
+        quickNav.append(`<li class="level-${level}"><a href="#${header_id}">${header_text}</a></li>`);
+    });
+    quickNavTrigger.on('click', function(e) {
+        $(this).siblings('ul').toggleClass('active');
+        e.stopPropagation();
+    });
+    quickNav.on('click', function(e) {
+        e.stopPropagation();
+    });
+    quickNav.find('li a').on('click', function() {
+        quickNav.removeClass('active');
+    });
+    $('body').on('click', function() {
+        quickNav.removeClass('active');
+    });
+});
