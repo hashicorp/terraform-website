a23020085c46112e68118bdd7a5d9737fa16f65b
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-04-03 17:16:48 -0700

    Minor cleanups
    Add comments, remove a single-use variable, change an ES6 template string to
    concatenation.

--- content/source/assets/javascripts/application.js
+++ content/source/assets/javascripts/application.js
@@ -23,27 +23,32 @@ document.addEventListener("turbolinks:load", function() {
     // H2/H3/H4 headers.
     var headers = $('#inner').find('h2, h3, h4');
     if (headers.length > 0) {
+        // Build the quick-nav HTML:
         $("#inner #inner-quicknav").html(
             '<span id="inner-quicknav-trigger">Page Quick Nav<svg width="9" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M8.811 1.067a.612.612 0 0 0 0-.884.655.655 0 0 0-.908 0L4.5 3.491 1.097.183a.655.655 0 0 0-.909 0 .615.615 0 0 0 0 .884l3.857 3.75a.655.655 0 0 0 .91 0l3.856-3.75z" fill-rule="evenodd"/></svg></span><ul class="dropdown"></ul>'
         );
-        var quickNavTrigger = $('#inner-quicknav #inner-quicknav-trigger');
         var quickNav = $('#inner-quicknav > ul.dropdown');
         headers.each(function(index, element) {
             var level = element.nodeName.toLowerCase();
             var header_text = $(element).text();
             var header_id = $(element).attr('id');
-            quickNav.append(`<li class="level-${level}"><a href="#${header_id}">${header_text}</a></li>`);
+            quickNav.append('<li class="level-' + level + '"><a href="#' + header_id + '">' + header_text + '</a></li>');
         });
-        quickNavTrigger.on('click', function(e) {
+        // Attach event listeners:
+        // Trigger opens and closes.
+        $('#inner-quicknav #inner-quicknav-trigger').on('click', function(e) {
             $(this).siblings('ul').toggleClass('active');
             e.stopPropagation();
         });
+        // Clicking inside the quick-nav doesn't close it.
         quickNav.on('click', function(e) {
             e.stopPropagation();
         });
+        // Jumping to a section means you're done with the quick-nav.
         quickNav.find('li a').on('click', function() {
             quickNav.removeClass('active');
         });
+        // Clicking outside the quick-nav closes it.
         $('body').on('click', function() {
             quickNav.removeClass('active');
         });
