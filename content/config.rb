f5706cf787839de619198e4f9b40569d23396ff5
Martin Atkins <mart@degeneration.co.uk>
2019-05-16 14:31:55 -0700

    Update config.rb

--- content/config.rb
+++ content/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.terraform.io/"
 
 activate :hashicorp do |h|
   h.name        = "terraform"
-  h.version     = "0.11.13"
+  h.version     = "0.11.14"
   h.github_slug = "hashicorp/terraform"
 end
 
