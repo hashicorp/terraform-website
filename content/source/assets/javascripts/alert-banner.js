8321e9129331250f0f0815e6211378aa7451b20b
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2020-06-25 12:43:19 -0400

    Bring in component logic
    Also, move the component to the very top of the page

--- content/source/assets/javascripts/alert-banner.js
+++ content/source/assets/javascripts/alert-banner.js
@@ -1,3 +1,49 @@
+//= require js-cookie
+
 document.addEventListener('turbolinks:load', function () {
-  // TODO Bring over the component logic
+  var qs = document.querySelector.bind(document)
+  var banner = qs('.g-alert-banner')
+  var closeButton = qs('.g-alert-banner .close')
+
+  // if cookie is set, don't show the component
+  if (cookie.get(`banner_terraform_io`)) {
+    closeButton.classList.remove('show')
+    return
+  }
+
+  closeButton.addEventListener(
+    'click',
+    () => {
+      onClose(banner)
+    },
+    { once: true }
+  )
 })
+
+function onClose(banner) {
+  // animate closed by setting height so
+  // it's not 'auto' and then set to zero
+  banner.style.height = `${banner.scrollHeight}px`
+  window.setTimeout(() => {
+    banner.style.height = '0'
+  }, 1)
+
+  // set the cookie so this banner doesn't show up anymore
+  const name = `banner_terraform_io`
+  cookie.set(name, 1)
+
+  // trackEvent('close')
+}
+
+function trackEvent(type) {
+  if (window.analytics) {
+    const { tag, theme, text, linkText } = { ...this.props }
+
+    window.analytics.track(type.charAt(0).toUpperCase() + type.slice(1), {
+      category: 'Alert Banner',
+      label: `${text} - ${linkText} | ${type}`,
+      tag: tag,
+      theme: theme,
+    })
+  }
+}
