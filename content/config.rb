ef573b7e8d30304a8be9bd0ca9dee471e95d4783
hashicorp-ci <hashicorp-ci@users.noreply.github.com>
2020-08-10 18:04:07 +0000

    Sync + Edit website

--- content/config.rb
+++ content/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.terraform.io/"
 
 activate :hashicorp do |h|
   h.name        = "terraform"
-  h.version     = "0.12.29"
+  h.version     = "0.13.0"
   h.github_slug = "hashicorp/terraform"
 end
 
