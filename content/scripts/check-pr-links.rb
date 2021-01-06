6b253a5d83b10dbbabdee7d099a57a460cf1fa94
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-01-05 19:14:49 -0800

    Rubocop changes, replacing a deprecated method

--- content/scripts/check-pr-links.rb
+++ content/scripts/check-pr-links.rb
@@ -3,6 +3,7 @@
 
 require 'nokogiri'
 require 'net/http'
+require 'erb'
 
 # Takes a list of source files to check on STDIN, and checks against a webserver
 # at localhost:4567. File list can be NULL-SEPARATED or NEWLINE-SEPARATED. You
@@ -38,20 +39,19 @@ def check_link(url)
 
   # Only 200s are "ok"; if it successfully redirects, you should still fix it anyway.
   if response.code == '200'
-    return [true, response.body]
+    [true, response.body]
   else
-    return [false, "[#{response.code} #{response.message.strip}] #{url}"]
+    [false, "[#{response.code} #{response.message.strip}] #{url}"]
   end
 rescue StandardError => e
-  # This won't be an HTTP error, it'll be a lower-level network problem: bad
-  # hostname, host is down, host is timing out, or whole network is hosed.
-  # Problem is, these errors can be SLOW, and I don't want the job to look
-  # stuck if we hit a wave of network problems. So in addition to reporting the
-  # error in its proper time, log to the console NOW so the user knows what's up.
+  # HTTP errors aren't exceptions, so this is a network problem: bad hostname,
+  # host is timing out, or whole network is hosed. These can be SLOW, and I
+  # don't want the job to look stuck. So in addition to reporting the error in
+  # its proper time, log to the console NOW so the user knows what's up.
   exception_message = "[#{e.class} - #{e.message.strip}] #{url}"
   puts "!! Got a network error: #{exception_message}"
-  puts "   Probably a bad hostname or a server timeout, but might be network trouble."
-  return [false, exception_message]
+  puts '   Probably a bad hostname or a server timeout, but might be network trouble.'
+  [false, exception_message]
 end
 
 # returns boolean
@@ -59,13 +59,13 @@ def check_anchor(html, anchor)
   page = Nokogiri::HTML(html)
   # anchor fragments can contain characters that are illegal in #id selectors (like '.'),
   # so we need to use an attribute selector with a quoted value instead.
-  return page.css("[id='#{anchor}']", "a[name='#{anchor}']").length > 0
+  !page.css("[id='#{anchor}']", "a[name='#{anchor}']").empty?
 rescue StandardError
   # Nokogiri throws errors on totally invalid selectors, so in the event that we
   # get some bizarro anchor that escapes the quoting or something in that
   # interpolation, just report it as a broken link instead of exploding the
   # whole run.
-  return false
+  false
 end
 
 # Returns array of link destination strings, which is probably a mix of relative
@@ -75,17 +75,21 @@ end
 # area is always relevant.
 def find_links(html)
   page = Nokogiri::HTML(html)
-  return page.css('#inner a').reject { |a| a.attributes['href'].nil? }.map { |a| a.attributes['href'].value }
+  page.css('#inner a').reject { |a| a.attributes['href'].nil? }.map { |a| a.attributes['href'].value }
 end
 
 input_files.each do |input_file|
   errors[input_file] = []
-  url_string = URI.escape(input_file).sub(site_root_paths, 'http://localhost:4567/').sub(page_extensions, '.html')
+  # Ruby has no stdlib equivalent of `encodeURI()`. There are several things like
+  # `encodeURIComponent()`, and ERB::Util.url_encode is the one that properly
+  # escapes spaces as %20.
+  url_encoded_filename = input_file.split('/').map { |s| ERB::Util.url_encode(s) }.join('/')
+  url_string = url_encoded_filename.sub(site_root_paths, 'http://localhost:4567/').sub(page_extensions, '.html')
   input_url = URI(url_string)
 
   ok, result = check_link(input_url)
 
-  if !ok
+  unless ok
     errors[input_file] << "  - Couldn't open page at all; something's extra-wrong.\n    #{result}"
     next
   end
@@ -93,10 +97,9 @@ input_files.each do |input_file|
   find_links(result).each do |link|
     # The URI class can just handle path traversal math for us, yay.
     link_url = URI.join(input_url, link)
-
     link_ok, link_result = check_link(link_url)
 
-    if !link_ok
+    unless link_ok
       errors[input_file] << "  - Broken link: #{link}\n    #{link_result}"
       next
     end
@@ -104,7 +107,7 @@ input_files.each do |input_file|
     anchor = link_url.fragment
     next unless anchor
 
-    if !check_anchor(link_result, anchor)
+    unless check_anchor(link_result, anchor)
       errors[input_file] << "  - Missing anchor: #{link} \n    (checked URL: #{link_url})"
     end
   end
