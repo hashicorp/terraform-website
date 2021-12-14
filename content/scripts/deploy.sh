65a0e88791ea605f07e183ee1fc1a6afd732fc63
Dylan Staley <88163+dstaley@users.noreply.github.com>
2021-12-13 16:33:22 -0800

    remove old middleman site

--- content/scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Deploys the site with a single command, for the rare manual push. When using
-# CI to build the site, you should instead configure uploading and cache-warming
-# as separate jobs, so that broken links don't mask a broken build.
-
-"${BASH_SOURCE%/*}/upload.sh"
-"${BASH_SOURCE%/*}/warm-cache.sh"
