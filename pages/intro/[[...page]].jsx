4ae882ff77300026ff45c42f33a36c3a8da9bae6
Dylan Staley <88163+dstaley@users.noreply.github.com>
2021-12-23 13:05:03 -0800

    replace plain img with next/image

--- pages/intro/[[...page]].jsx
+++ pages/intro/[[...page]].jsx
@@ -6,6 +6,12 @@ import {
   generateStaticPaths,
   generateStaticProps,
 } from '@hashicorp/react-docs-page/server'
+import Image from 'next/image'
+import visit from 'unist-util-visit'
+import imageSizeOf from 'image-size'
+import { promisify } from 'util'
+
+const sizeOf = promisify(imageSizeOf)
 
 //  Configure the docs path
 const BASE_ROUTE = 'intro'
@@ -23,6 +29,7 @@ export default function IntroLayout(props) {
       baseRoute={BASE_ROUTE}
       product={PRODUCT}
       staticProps={modifiedProps}
+      additionalComponents={{ Image }}
     />
   )
 }
@@ -45,6 +52,53 @@ export async function getStaticProps({ params }) {
       const filepath = path.replace('content/', '')
       return `https://github.com/hashicorp/${PRODUCT.slug}/blob/main/website/${filepath}`
     },
+    remarkPlugins: [
+      () => {
+        return async function transform(tree) {
+          const promises = []
+
+          visit(tree, 'image', (node, index, parent) => {
+            // Only use next/image for local images
+            if (!node.url.startsWith('http')) {
+              const promise = (async () => {
+                try {
+                  // Assume that images are stored in public/
+                  const relativePath = `./public${node.url}`
+                  // Get image dimensions
+                  const { width, height } = await sizeOf(relativePath)
+                  const props = [
+                    ['src', `"${node.url}"`],
+                    ['alt', `"${node.alt}"`],
+                    ['width', `{${width}}`],
+                    ['height', `{${height}}`],
+                  ]
+                    .map((p) => p.join('='))
+                    .join(' ')
+                  // Build the replacement JSX node
+                  const replacementNode = {
+                    type: 'jsx',
+                    value: `<Image ${props} />`,
+                  }
+
+                  // Replace original img element with the JSX node
+                  parent.children.splice(index, 1, replacementNode)
+                } catch (err) {
+                  // If we encounter any error, log it, but leave the img
+                  // tag in its original state.
+                  console.warn(err)
+                }
+              })()
+
+              // Collect all promises.
+              promises.push(promise)
+            }
+          })
+
+          // Await all promises.
+          await Promise.all(promises)
+        }
+      },
+    ],
   })
   return { props }
 }
