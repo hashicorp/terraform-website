5113cb8d5fa5846bb3fd9c46844ae6eaf0a39050
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-01-04 11:21:48 -0500

    fix: internal link check

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,8 +2,8 @@ export const isInternalLink = (link: string): boolean => {
   if (
     link.startsWith('/') ||
     link.startsWith('#') ||
-    link.startsWith('https://consul.io') ||
-    link.startsWith('https://www.consul.io')
+    link.startsWith('https://terraform.io') ||
+    link.startsWith('https://www.terraform.io')
   ) {
     return true
   }
