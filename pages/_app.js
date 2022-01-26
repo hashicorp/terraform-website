d80755459a9dd1179088f4bc9392b222f702a85c
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-01-26 13:49:39 -0500

    fix: Add services to the manager options

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import './style.css'
 import '@hashicorp/platform-util/nprogress/style.css'
 
 import NProgress from '@hashicorp/platform-util/nprogress'
-// import createConsentManager from '@hashicorp/react-consent-manager/loader'
+// import createConsentManager from '@hashicorp/react-consent-manager'
+// import localConsentManagerServices from 'lib/consent-manager-services'
 import usePageviewAnalytics from '@hashicorp/platform-analytics'
 import useAnchorLinkAnalytics from '@hashicorp/platform-util/anchor-link-analytics'
 import Router from 'next/router'
@@ -19,6 +20,7 @@ import alertBannerData, { ALERT_BANNER_ACTIVE } from 'data/alert-banner'
 NProgress({ Router })
 // const { ConsentManager, openConsentManager } = createConsentManager({
 //   preset: 'oss',
+//   otherServices: [...localConsentManagerServices]
 // })
 
 function App({ Component, pageProps }) {
