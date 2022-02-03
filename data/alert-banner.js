f85e5ca15a0f590a6cbc9b3897395f96a264e281
Kyle MacDonald <macdonaldky@gmail.com>
2022-02-03 13:40:39 -0500

    extend alert banner for addtl week

--- data/alert-banner.js
+++ data/alert-banner.js
@@ -8,5 +8,5 @@ export default {
   linkText: 'Take our survey',
   // Set the expirationDate prop with a datetime string (e.g. '2020-01-31T12:00:00-07:00')
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2022-02-04T23:00:00-07:00',
+  expirationDate: '2022-02-10T23:00:00-07:00',
 }
