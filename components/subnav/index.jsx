7fab4376dc7fe89f35d8cda76b8ffa4b7b9ffb32
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-02-18 08:24:25 -0500

    chore: bump subnav package

--- components/subnav/index.jsx
+++ components/subnav/index.jsx
@@ -5,12 +5,15 @@ import navMenuItems from 'data/primary-navigation.js'
 export default function DefaultSubnav() {
   return (
     <Subnav
-      titleLink={{ text: 'terraform', url: '/' }}
+      titleLink={{ text: 'HashiCorp Terraform', url: '/' }}
       ctaLinks={[
         { text: 'Download CLI', url: '/downloads' },
         {
           text: 'Terraform Cloud',
           url: 'https://cloud.hashicorp.com/products/terraform',
+          theme: {
+            brand: 'terraform',
+          },
         },
       ]}
       menuItems={navMenuItems}
