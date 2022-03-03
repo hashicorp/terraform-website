5e17b0ec4b2c87f6f7371942a1224195563f90a2
Bryce Kalow <bkalow@hashicorp.com>
2022-03-03 09:04:04 -0600

    adjust remark plugin

--- pages/cdktf/[[...page]].jsx
+++ pages/cdktf/[[...page]].jsx
@@ -12,8 +12,8 @@ import path from 'path'
 
 //  Configure the docs path
 const BASE_ROUTE = 'cdktf'
-const NAV_DATA = '../data/cdktf-nav-data.json'
-const CONTENT_DIR = '../docs/cdktf'
+const NAV_DATA = process.env.NAV_DATA_PATH || '../data/cdktf-nav-data.json'
+const CONTENT_DIR = process.env.CONTENT_DIR || '../docs/cdktf'
 const PRODUCT = { name: productName, slug: 'terraform-cdk' }
 
 export default function CDKLayout(props) {
@@ -39,6 +39,26 @@ const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions(
         localContentDir: CONTENT_DIR,
         navDataFile: NAV_DATA,
         product: PRODUCT.slug,
+        remarkPlugins: [
+          () => {
+            const product = 'terraform-cdk'
+            const version = 'main'
+            return function transform(tree) {
+              visit(tree, 'image', (node) => {
+                const assetPath = params.page
+                  ? path.posix.join(
+                      ...params.page,
+                      node.url.startsWith('.')
+                        ? `.${node.url}`
+                        : `../${node.url}`
+                    )
+                  : node.url
+                const asset = path.posix.join('website/docs/cdktf', assetPath)
+                node.url = `https://mktg-content-api.vercel.app/api/assets?product=${product}&version=${version}&asset=${asset}`
+              })
+            }
+          },
+        ],
       }
     : {
         strategy: 'remote',
