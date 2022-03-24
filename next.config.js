246a00e510762773708497cb74da2077fcb5985f
Kevin Wang <kwangsan@gmail.com>
2022-03-24 14:10:39 -0400

    remove comments

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+// const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 const path = require('path')
 const redirects = require('./redirects.next.js')
 
@@ -10,15 +10,8 @@ module.exports = (phase, { defaultConfig }) => {
     BUGSNAG_CLIENT_KEY: 'eab8d5350ab3b12d77b7498b23f9a89a',
     BUGSNAG_SERVER_KEY: '1f55a49019f70f94a17dd6d93210f09d',
     IS_CONTENT_PREVIEW: process.env.IS_CONTENT_PREVIEW || true,
-    NAV_DATA_PATH: process.env.NAV_DATA_PATH || '../../../website/data/cdktf-nav-data.json',
-    CONTENT_DIR: process.env.CONTENT_DIR || "../../../website/docs/cdktf",
-  }
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    console.log()
-    console.log("==================phase: ", phase)
-    console.log(env)
-    console.log()
+    NAV_DATA_PATH: process.env.NAV_DATA_PATH,
+    CONTENT_DIR: process.env.CONTENT_DIR,
   }
 
   /**
