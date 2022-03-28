ca8743dd00494ddcc866ead1e939fe26964ca75c
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-28 12:19:31 -0400

    fix: use filterVersions from hashicorp/dev-portal#231

--- pages/downloads/index.jsx
+++ pages/downloads/index.jsx
@@ -1,8 +1,3 @@
-import semverGte from 'semver/functions/gte'
-import semverMajor from 'semver/functions/major'
-import semverMinor from 'semver/functions/minor'
-import semverPatch from 'semver/functions/patch'
-
 import VERSION from 'data/version'
 import { productSlug } from 'data/metadata'
 import Logo from '@hashicorp/mktg-assets/dist/product/terraform-logo/color'
@@ -10,8 +5,9 @@ import ProductDownloadsPage from '@hashicorp/react-product-downloads-page'
 import Button from '@hashicorp/react-button'
 import { generateStaticProps } from '@hashicorp/react-product-downloads-page/server'
 import s from './style.module.css'
+import filterVersions from 'lib/filter-versions'
 
-const VERSION_DOWNLOAD_CUTOFF = '1.0.11'
+const VERSION_DOWNLOAD_CUTOFF = '>=1.0.11'
 
 export default function DownloadsPage(staticProps) {
   return (
@@ -52,58 +48,16 @@ export default function DownloadsPage(staticProps) {
   )
 }
 
-function filterOldVersions(props) {
-  if (!props?.props?.releases?.versions) return props
-
-  const versions = props.props.releases.versions
-
-  // versions is in the form of { [version]: { ...metadata } }
-  // Filter by arbitrary & reasonable version cutoff
-  const filteredVersions = Object.keys(versions).filter((version) => {
-    if (!semverGte(version, VERSION_DOWNLOAD_CUTOFF)) return false
-    return true
-  })
-
-  /** @type {{[x: string]:{ [y: string]: any}}} */
-  const tree = {}
-
-  /**
-   * Computes the latest patch versions for each major/minor
-   * e.g. given [1.1.2, 1.1.1, 1.1.0, 1.0.9, 1.0.8] -> return [1.1.2, 1.0.9]
-   */
-  filteredVersions.forEach((v) => {
-    const x = semverMajor(v)
-    const y = semverMinor(v)
-    const z = semverPatch(v)
-
-    if (!tree[x]) {
-      tree[x] = { [y]: z }
-    } else if (!tree[x][y]) {
-      tree[x][y] = z
-    } else {
-      tree[x][y] = Math.max(tree[x][y], z)
-    }
-  })
-
-  const newVersions = {}
-
-  Object.entries(tree).forEach(([x, xObj]) => {
-    Object.entries(xObj).forEach(([y, z]) => {
-      const version = `${x}.${y}.${z}`
-      newVersions[version] = versions[version]
-    })
-  })
-
-  props.props.releases.versions = newVersions
-}
-
 export async function getStaticProps() {
   const props = await generateStaticProps({
     product: productSlug,
     latestVersion: VERSION,
   })
 
-  filterOldVersions(props)
+  // Filter versions based on VERSION_DOWNLOAD_CUTOFF
+  const rawVersions = props.props?.releases?.versions
+  const filteredVersions = filterVersions(rawVersions, VERSION_DOWNLOAD_CUTOFF)
+  props.props.releases.versions = filteredVersions
 
   return props
 }
