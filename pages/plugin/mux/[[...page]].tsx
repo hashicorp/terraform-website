0227159099098543ed2ba76a545e93ab07d628be
Kevin Wang <kwangsan@gmail.com>
2022-05-02 18:21:44 -0400

    feat: remote content

--- pages/plugin/mux/[[...page]].tsx
+++ pages/plugin/mux/[[...page]].tsx
@@ -5,15 +5,21 @@ import otherPluginsData from 'data/other-plugins-nav-data.json'
 
 // Imports below are only used server-side
 import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server'
+import path from 'path'
 
 //  Configure the docs path
 const BASE_ROUTE = 'plugin/mux'
-const NAV_DATA = 'data/plugin-mux-nav-data.json'
-const CONTENT_DIR = 'content/plugin/mux'
+// const NAV_DATA = 'data/plugin-mux-nav-data.json'
+const NAV_DATA = path.join(
+  process.env.NAV_DATA_DIRNAME,
+  'plugin-mux' + '-nav-data.json'
+)
+// const CONTENT_DIR = 'content/plugin/mux'
+const CONTENT_DIR = path.join(process.env.CONTENT_DIRNAME, BASE_ROUTE)
 const PRODUCT = { name: productName, slug: 'terraform' } as const
 
-// TODO: update to terraform-plugin-mux
-const SOURCE_REPO = 'terraform-website'
+const SOURCE_REPO = 'terraform-plugin-mux'
+const DEFAULT_BRANCH = 'main'
 
 export default function PluginMuxLayout(props) {
   // display "Other Plugin Docs" section
@@ -35,14 +41,23 @@ export default function PluginMuxLayout(props) {
   )
 }
 
-const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
-  strategy: 'fs',
-  localContentDir: CONTENT_DIR,
-  navDataFile: NAV_DATA,
-  product: SOURCE_REPO,
-  githubFileUrl(path) {
-    return `https://github.com/hashicorp/${SOURCE_REPO}/blob/master/${path}`
-  },
-})
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions(
+  process.env.IS_CONTENT_PREVIEW &&
+    process.env.PREVIEW_FROM_REPO === SOURCE_REPO
+    ? {
+        strategy: 'fs',
+        localContentDir: CONTENT_DIR,
+        navDataFile: NAV_DATA,
+        product: SOURCE_REPO,
+        githubFileUrl(path) {
+          return `https://github.com/hashicorp/${SOURCE_REPO}/blob/${DEFAULT_BRANCH}/${path}`
+        },
+      }
+    : {
+        strategy: 'remote',
+        basePath: BASE_ROUTE,
+        product: SOURCE_REPO,
+      }
+)
 
 export { getStaticPaths, getStaticProps }
