40fb0bc9846e2e9645164be1f560103996e7a8bf
Kevin Wang <kwangsan@gmail.com>
2022-05-03 11:28:18 -0400

    feat: `/plugin/log` remote content

--- pages/plugin/log/[[...page]].tsx
+++ pages/plugin/log/[[...page]].tsx
@@ -5,15 +5,23 @@ import otherPluginsData from 'data/other-plugins-nav-data.json'
 
 // Imports below are only used server-side
 import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server'
+import path from 'path'
+
+import { remarkRewriteAssets } from 'lib/remark-rewrite-assets'
 
 //  Configure the docs path
 const BASE_ROUTE = 'plugin/log'
-const NAV_DATA = 'data/plugin-log-nav-data.json'
-const CONTENT_DIR = 'content/plugin/log'
+// const NAV_DATA = 'data/plugin-log-nav-data.json'
+const NAV_DATA = path.join(
+  process.env.NAV_DATA_DIRNAME,
+  'plugin-log' + '-nav-data.json'
+)
+// const CONTENT_DIR = 'content/plugin/log'
+const CONTENT_DIR = path.join(process.env.CONTENT_DIRNAME, BASE_ROUTE)
 const PRODUCT = { name: productName, slug: 'terraform' } as const
 
-// TODO: update to terraform-plugin-log
-const SOURCE_REPO = 'terraform-website'
+const SOURCE_REPO = 'terraform-plugin-log'
+const DEFAULT_BRANCH = 'main'
 
 export default function PluginLogLayout(props) {
   // display "Other Plugin Docs" section
@@ -34,14 +42,30 @@ export default function PluginLogLayout(props) {
   )
 }
 
-const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
-  strategy: 'fs',
-  localContentDir: CONTENT_DIR,
-  navDataFile: NAV_DATA,
-  product: SOURCE_REPO,
-  githubFileUrl(path) {
-    return `https://github.com/hashicorp/${SOURCE_REPO}/blob/master/${path}`
-  },
-})
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions(
+  process.env.IS_CONTENT_PREVIEW &&
+    process.env.PREVIEW_FROM_REPO === SOURCE_REPO
+    ? {
+        strategy: 'fs',
+        localContentDir: CONTENT_DIR,
+        navDataFile: NAV_DATA,
+        product: SOURCE_REPO,
+        githubFileUrl(filepath) {
+          return `https://github.com/hashicorp/${SOURCE_REPO}/blob/${DEFAULT_BRANCH}/${filepath}`
+        },
+        remarkPlugins: (params) => [
+          remarkRewriteAssets({
+            product: SOURCE_REPO,
+            version: process.env.CURRENT_GIT_BRANCH,
+            getAssetPathParts: (nodeUrl) => ['website', nodeUrl],
+          }),
+        ],
+      }
+    : {
+        strategy: 'remote',
+        basePath: BASE_ROUTE,
+        product: SOURCE_REPO,
+      }
+)
 
 export { getStaticPaths, getStaticProps }
