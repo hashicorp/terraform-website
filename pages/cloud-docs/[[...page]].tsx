047eddd6fa6be9cfbe5e702a41102600d2f07c18
Kevin Wang <kwangsan@gmail.com>
2022-05-18 11:39:14 -0400

    feat: `cloud-docs` + `registry` remote content

--- pages/cloud-docs/[[...page]].tsx
+++ pages/cloud-docs/[[...page]].tsx
@@ -3,16 +3,21 @@ import DocsPage from '@hashicorp/react-docs-page'
 import otherDocsData from 'data/other-docs-nav-data.json'
 // Imports below are only used server-side
 import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server'
-import { NextParsedUrlQuery } from 'next/dist/server/request-meta'
+import path from 'path'
+
+import { remarkRewriteAssets } from 'lib/remark-rewrite-assets'
 
 //  Configure the docs path
 const BASE_ROUTE = 'cloud-docs'
-const NAV_DATA = 'data/cloud-docs-nav-data.json'
-const CONTENT_DIR = 'content/cloud-docs'
+const NAV_DATA = path.join(
+  process.env.NAV_DATA_DIRNAME,
+  BASE_ROUTE + '-nav-data.json'
+)
+const CONTENT_DIR = path.join(process.env.CONTENT_DIRNAME, BASE_ROUTE)
 const PRODUCT = { name: productName, slug: 'terraform' } as const
 
-const SOURCE_REPO = 'terraform-website'
-const DEFAULT_BRANCH = 'master'
+const SOURCE_REPO = 'terraform-docs-common'
+const DEFAULT_BRANCH = 'main'
 
 export default function CloudDocsLayout(props) {
   // add the "other docs" section to the bottom of the nav data
@@ -28,28 +33,32 @@ export default function CloudDocsLayout(props) {
   )
 }
 
-const { getStaticPaths: _getStaticPaths, getStaticProps } =
-  getStaticGenerationFunctions({
-    strategy: 'fs',
-    localContentDir: CONTENT_DIR,
-    navDataFile: NAV_DATA,
-    product: SOURCE_REPO,
-    githubFileUrl(filepath) {
-      return `https://github.com/hashicorp/${SOURCE_REPO}/blob/${DEFAULT_BRANCH}/${filepath}`
-    },
-  })
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions(
+  process.env.IS_CONTENT_PREVIEW &&
+    process.env.PREVIEW_FROM_REPO === SOURCE_REPO
+    ? {
+        strategy: 'fs',
+        localContentDir: CONTENT_DIR,
+        navDataFile: NAV_DATA,
+        product: SOURCE_REPO,
+        githubFileUrl(filepath) {
+          // This path rewriting is meant for local preview from `terraform-docs-common`.
+          filepath = filepath.replace('preview/', '')
+          return `https://github.com/hashicorp/${SOURCE_REPO}/blob/${DEFAULT_BRANCH}/website/${filepath}`
+        },
+        remarkPlugins: (params) => [
+          remarkRewriteAssets({
+            product: SOURCE_REPO,
+            version: process.env.CURRENT_GIT_BRANCH,
+            getAssetPathParts: (nodeUrl) => ['website', nodeUrl],
+          }),
+        ],
+      }
+    : {
+        strategy: 'remote',
+        basePath: BASE_ROUTE,
+        product: SOURCE_REPO,
+      }
+)
 
-/**
- * TODO (kevinwang): This is a temporary hack until /cloud-docs sources remote content.
- */
-const getStaticPaths = async (ctx) => {
-  const res = await _getStaticPaths(ctx)
-  const paths = res.paths.filter((p: { params: NextParsedUrlQuery }) => {
-    return !/agents/i.test(p.params.page?.[0])
-  })
-
-  // update getStaticPaths object before returning it to Next.js
-  res.paths = paths
-  return res
-}
 export { getStaticPaths, getStaticProps }
