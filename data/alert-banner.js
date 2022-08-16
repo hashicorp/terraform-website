c2b45ffc039c1552f873bb10e11df45ef53c8a42
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-08-16 17:04:24 -0400

    chore: 'virtual' -> 'online'

--- data/alert-banner.js
+++ data/alert-banner.js
@@ -4,7 +4,7 @@ export const ALERT_BANNER_ACTIVE = true
 export default {
   tag: 'HashiConf Global',
   url: 'https://hashiconf.com/global/?_gl=1*u898mc*_ga*Nzg2MzExNjMwLjE2NDQ5NDYzMDU.*_ga_P7S46ZYEKW*MTY1OTk4NjE2NC4xMjIuMS4xNjU5OTg5MjIxLjA.',
-  text: 'Join our HashiCorp community October 4-6 in Los Angeles, CA & virtual.',
+  text: 'Join our HashiCorp community October 4-6 in Los Angeles, CA & online.',
   linkText: 'Register Now',
   expirationDate: '',
 }
