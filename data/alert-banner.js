85264539c22381a57fbb04aa5bbae0b3a9408083
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-08-16 14:43:36 -0400

    feat: Update alert banner

--- data/alert-banner.js
+++ data/alert-banner.js
@@ -2,9 +2,9 @@ export const ALERT_BANNER_ACTIVE = false
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  "tag": "June 20-22",
-  "url": "https://hashiconf.com/europe?utm_source=Banner",
-  "text": "Announcing HashiConf Europe full schedule: keynotes, sessions, labs & more",
-  "linkText": "Register Now",
-  "expirationDate": ""
+  tag: 'HashiConf Global',
+  url: 'https://hashiconf.com/global/?_gl=1*u898mc*_ga*Nzg2MzExNjMwLjE2NDQ5NDYzMDU.*_ga_P7S46ZYEKW*MTY1OTk4NjE2NC4xMjIuMS4xNjU5OTg5MjIxLjA.',
+  text: 'Join our HashiCorp community October 4-6 in Los Angeles, CA & virtual.',
+  linkText: 'Register Now',
+  expirationDate: '',
 }
