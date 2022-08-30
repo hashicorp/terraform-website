4725c1db6a0e9960c00740e5d02c8de3c11bf74a
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-08-30 13:32:49 -0400

    chore: Update copy/add UTMs

--- data/alert-banner.js
+++ data/alert-banner.js
@@ -2,9 +2,9 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'HashiConf Global',
-  url: 'https://hashiconf.com/global',
-  text: 'Full schedule now available: keynotes, sessions, labs & more.',
+  tag: 'Just announced',
+  url: 'https://hashiconf.com/global/?utm_source=direct&utm_medium=banner&utm_campaign=23Q3_HASHICONFGLOBAL_EVENT&utm_content=&utm_offer=event',
+  text: 'HashiConf Global full schedule: keynotes, sessions, labs & more.',
   linkText: 'Register Now',
   expirationDate: '',
 }
