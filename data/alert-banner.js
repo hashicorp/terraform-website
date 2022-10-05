5902b98ac3536db2736a767566f3b65d0c2822b5
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-10-05 10:57:16 -0400

    feat(AlertBanner): Update for HashiConf Global opening (#2417)

--- data/alert-banner.js
+++ data/alert-banner.js
@@ -2,9 +2,9 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'Just announced',
+  tag: 'October 5-6',
   url: 'https://hashiconf.com/global/?utm_source=direct&utm_medium=banner&utm_campaign=23Q3_HASHICONFGLOBAL_EVENT&utm_content=&utm_offer=event',
-  text: 'HashiConf Global full schedule: keynotes, sessions, labs & more.',
-  linkText: 'Register Now',
+  text: 'HashiConf Global is live. Join us virtually for keynotes, sessions, and more.',
+  linkText: 'Join now',
   expirationDate: '',
 }
