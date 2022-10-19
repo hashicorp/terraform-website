1835408ad2450bcc984e251bb888c5b489da5cd8
Kevin Wang <kwangsan@gmail.com>
2022-10-19 17:34:33 -0400

    Update config.ts

--- flags/config.ts
+++ flags/config.ts
@@ -4,7 +4,8 @@ import type { Configuration } from '@happykit/flags/config'
 export type AppFlags = { [key: string]: boolean | number | string | null }
 
 export const config: Configuration<AppFlags> = {
-  envKey: process.env.NEXT_PUBLIC_FLAGS_ENV_KEY!,
+  // see next.config.js for env var value
+  envKey: process.env.HAPPY_KIT_KEY!,
 
   // You can provide defaults flag values here
   defaultFlags: {
