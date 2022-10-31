1e0e409e83fd2839e7a5b4f98fe51e8d69d6ce6a
Laura Pacilio <83350965+laurapacilio@users.noreply.github.com>
2022-10-31 11:40:06 -0400

    Add temporary redirects to exclude OPA from TFE (#2434)
    * Add temporary redirects to exclude OPA from TFE
    
    * update redirects
    
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>

--- redirects.js
+++ redirects.js
@@ -2,9 +2,27 @@
  * Redirects in this file are intended to be for documentation content only. The redirects will be applied to developer.hashicorp.com.
  */
 module.exports = [
-  // {
-  //   source: '/terraform/docs/path',
-  //   destination: '/terraform/docs/new/path',
-  //   permanent: true,
-  // },
+  // Temporary Redirects to defer new/moved cloud-docs pages to their older respective enterprise pages
+  // - https://github.com/hashicorp/terraform-docs-common-internal/pull/6
+  // - https://github.com/hashicorp/terraform-docs-common/pull/141
+  {
+    source: '/terraform/enterprise/policy-enforcement',
+    destination: '/terraform/enterprise/sentinel',
+    permanent: false,
+  },
+  {
+    source: '/terraform/enterprise/policy-enforcement/sentinel',
+    destination: '/terraform/enterprise/sentinel',
+    permanent: false,
+  },
+  {
+    source: '/terraform/enterprise/policy-enforcement/:path*',
+    destination: '/terraform/enterprise/sentinel/:path*',
+    permanent: false,
+  },
+  {
+    source: '/terraform/enterprise/policy-enforcement/sentinel/:path*',
+    destination: '/terraform/enterprise/sentinel/:path*',
+    permanent: false,
+  },
 ]
