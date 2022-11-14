089b29c4ef5f782fce104e52fcbcce49bad780d6
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-11-14 11:53:01 -0500

    chore: scope experiments to US (#2442)
    * scope experiments to US
    
    * fix: casing:

--- middleware.ts
+++ middleware.ts
@@ -27,7 +27,7 @@ const BASE_PATHS = [
   'registry',
 ]
 
-const devDotRoutes = [ ...BASE_PATHS, 'downloads']
+const devDotRoutes = [...BASE_PATHS, 'downloads']
 const devDotRedirectCheck = new RegExp(`^/(${devDotRoutes.join('|')})/?`)
 
 function setHappyKitCookie(
@@ -40,6 +40,7 @@ function setHappyKitCookie(
 
 export default async function middleware(request: NextRequest) {
   let response: NextResponse
+  const { geo } = request
 
   /**
    * Apply redirects to nested docs pages from a static list in data/docs-redirects.
@@ -77,7 +78,7 @@ export default async function middleware(request: NextRequest) {
   /**
    * We are running A/B tests on a subset of routes, so we are limiting the call to resolve flags from HappyKit to only those routes. This limits the impact of any additional latency to the routes which need the data.
    */
-  if (['/'].includes(request.nextUrl.pathname)) {
+  if (geo?.country === 'US' && ['/'].includes(request.nextUrl.pathname)) {
     try {
       const edgeFlags = await getEdgeFlags({ request })
       const { flags, cookie } = edgeFlags
