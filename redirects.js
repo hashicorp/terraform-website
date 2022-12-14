406bc43c46f4aa53d48daa4a87a1f95aaa41f930
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-12-14 16:00:50 +0000

    Adding redirects for restructured Terraform Plugin Framework docs (#2449)
    * Adding redirects for restructured Terraform Plugin Framework docs prior to GA release of the Framework (#2448)
    
    * Updating redirects (#2448)

--- redirects.js
+++ redirects.js
@@ -25,4 +25,36 @@ module.exports = [
     destination: '/terraform/enterprise/sentinel/:path*',
     permanent: false,
   },
+  // Redirects for restructured Terraform Plugin Framework docs for GA release of the Framework
+  // - https://github.com/hashicorp/terraform-plugin-framework/pull/554
+  {
+    source: '/terraform/plugin/framework/schemas',
+    destination: '/terraform/plugin/framework/handling-data/schemas',
+    permanent: true,
+  },
+  {
+    source: '/terraform/plugin/framework/types',
+    destination: '/terraform/plugin/framework/handling-data/attributes',
+    permanent: true,
+  },
+  {
+    source: '/terraform/plugin/framework/paths',
+    destination: '/terraform/plugin/framework/handling-data/paths',
+    permanent: true,
+  },
+  {
+    source: '/terraform/plugin/framework/path-expressions',
+    destination: '/terraform/plugin/framework/handling-data/path-expressions',
+    permanent: true,
+  },
+  {
+    source: '/terraform/plugin/framework/accessing-values',
+    destination: '/terraform/plugin/framework/handling-data/accessing-values',
+    permanent: true,
+  },
+  {
+    source: '/terraform/plugin/framework/writing-state',
+    destination: '/terraform/plugin/framework/handling-data/writing-state',
+    permanent: true,
+  },
 ]
