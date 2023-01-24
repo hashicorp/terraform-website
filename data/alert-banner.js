260ada4683cef1cbe6e4fb99bfed721d402ada15
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-01-24 09:31:26 -0500

    chore: add hashitalks alert banner

--- data/alert-banner.js
+++ data/alert-banner.js
@@ -1,10 +1,10 @@
-export const ALERT_BANNER_ACTIVE = false
+export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'October 5-6',
-  url: 'https://hashiconf.com/global/?utm_source=direct&utm_medium=banner&utm_campaign=23Q3_HASHICONFGLOBAL_EVENT&utm_content=&utm_offer=event',
-  text: 'HashiConf Global is live. Join us virtually for keynotes, sessions, and more.',
-  linkText: 'Join now',
+  tag: 'HashiTalks 2023',
+  url: 'https://hashi.co/hashitalks-2023',
+  text: 'Join us for the fifth annual HashiTalks, a 24-hour virtual knowledge sharing event on February 16',
+  linkText: 'Register now',
   expirationDate: '',
 }
