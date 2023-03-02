0d1031f25126db0e1ed1727f71fa2ae811ef7d9a
Bryce Kalow <bkalow@hashicorp.com>
2023-03-02 15:56:45 -0600

    remove unused opt-in component

--- components/docs-page/index.tsx
+++ components/docs-page/index.tsx
@@ -1,6 +1,5 @@
 import DocsPage from '@hashicorp/react-docs-page'
-import DevDotOptIn from 'components/dev-dot-opt-in'
 
 export default function TerraformDocsPage(props) {
-  return <DocsPage {...props} optInBanner={<DevDotOptIn />} />
+  return <DocsPage {...props} />
 }
