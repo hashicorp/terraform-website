5f48a6efb1fa9fa350892ec1ce7e1868b08a0ef6
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-04-24 11:13:27 -0400

    chore: removes experiment code (#2484)

--- components/io-home-hero-alt/index.tsx
+++ components/io-home-hero-alt/index.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react'
-import classNames from 'classnames'
-import { useFlagBag } from 'flags/client'
 import Image from 'next/image'
-import { abTestTrack } from 'lib/ab-test-track'
 import type { Products } from '@hashicorp/platform-product-meta'
 import type { IntroProps } from '@hashicorp/react-intro/types'
-import { isInUS } from '@hashicorp/platform-util/geo'
 import Intro from '@hashicorp/react-intro'
-import Button from '@hashicorp/react-button'
-import StandaloneLink from '@hashicorp/react-standalone-link'
 import s from './style.module.css'
 
 interface IoHomeHeroAltProps {
@@ -24,10 +18,6 @@ export default function IoHomeHeroAlt({
   description,
   ctas,
 }: IoHomeHeroAltProps) {
-  const flagBag = useFlagBag()
-  const renderVariant = React.useMemo(() => {
-    return isInUS() && flagBag.settled && flagBag.flags?.tryForFree
-  }, [flagBag])
   return (
     <header className={s.hero}>
       <div className={s.patterns}>
@@ -57,54 +47,19 @@ export default function IoHomeHeroAlt({
             heading={heading}
             headingSize={1}
             description={description}
-            // Temporary use custom actions implementation for experiement.
-            //
-            // actions={{
-            //   layout: 'stacked',
-            //   theme: brand,
-            //   ctas: ctas.map(
-            //     (cta: { title: string; href: string }, index: number) => {
-            //       return {
-            //         ...cta,
-            //         type: index === 0 ? 'button' : 'standalone-link',
-            //       }
-            //     }
-            //   ) as IntroProps['actions']['ctas'],
-            // }}
+            actions={{
+              layout: 'stacked',
+              theme: brand,
+              ctas: ctas.map(
+                (cta: { title: string; href: string }, index: number) => {
+                  return {
+                    ...cta,
+                    type: index === 0 ? 'button' : 'standalone-link',
+                  }
+                }
+              ) as IntroProps['actions']['ctas'],
+            }}
           />
-          <div className={s.actions}>
-            {ctas.map((cta, index) => {
-              if (index === 0) {
-                return (
-                  <Button
-                    key={cta.href}
-                    title={
-                      renderVariant ? 'Try for free' : 'Try Terraform Cloud'
-                    }
-                    url={cta.href}
-                    theme={{ brand }}
-                    onClick={() => {
-                      abTestTrack({
-                        type: 'Result',
-                        test_name: 'io-site primary CTA copy test 03-23',
-                        variant: flagBag.flags?.tryForFree.toString(),
-                      })
-                    }}
-                    className={classNames(
-                      s.actionsPrimary,
-                      flagBag.settled && s.settled
-                    )}
-                  />
-                )
-              }
-
-              return (
-                <StandaloneLink href={cta.href} theme="secondary">
-                  {cta.title}
-                </StandaloneLink>
-              )
-            })}
-          </div>
         </div>
       </div>
     </header>
